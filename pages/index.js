cc16fba0c4de25f740bc6e68688c7ce54a1d92a8
lemao-code <th.ferrari@outlook.com.br>
2021-01-19 21:52:25 -0300

    tag head

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import { useEffect } from 'react'
-import {  useRouter } from 'next/router'
-import {Container, HeaderChess, ContainerChildren} from '../style/style'
+
+import {Container, ContainerChildren} from '../style/style'
+import {Head} from 'next/document'
 import Image from 'next/image'
 import Ranking from '../components/Ranking'
 import LastMatches from '../components/LastMatches'
 import AddMatch from '../components/AddMatch'
-import * as gtag from '../lib/gtag'
 
 function Home () {
 
-    <script data-ad-client="ca-pub-7702738899679859" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+     
     return (
+        
         <div style={{padding: 0, marginLeft:-10, marginTop: -20, marginBottom: -10, backgroundColor: "black", width:"120%", height: "auto"}}>
+            <Head>
+            <script data-ad-client="ca-pub-7702738899679859" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        </Head>
             <Container >
           
                     <Image src="/chess.webp" quality="100" responsive="true" width="1200" height="600"/>
